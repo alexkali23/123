5d8432083a8dce1167fdd4251ef63a5728f43fde
alexkali23 <50529116+alexkali23@users.noreply.github.com>
2019-09-22 18:58:31 +0300

    Update settings.py

--- dotadream/dotadream/settings.py
+++ dotadream/dotadream/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
-STATICFILES_DIRS = (
-     os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+#)
 STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 
 STATIC_URL = '/static/'
